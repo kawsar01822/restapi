0f34b7cc444cd3320e1471e0bca705f7617146c6
Kawsar Ahmed <kaws.med@gmail.com>
2022-10-26 00:02:40 +0600

    Delete restapi directory

--- restapi/app/Helpers/form_helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function display_error($errors,$field){
-        if($errors->hasError($field)){
-            return $errors->getError($field);
-        }else{
-            return false;
-        }
-    }
-?>
--- restapi/app/Views/dashboard.php
+++
@@ -1,7 +0,0 @@
-<?= $this->extend('static/base'); ?>
-
-<?= $this->section('content'); ?>
-
-    <h1>--- Dashboard ---</h1>
-
-<?= $this->endSection(); ?>
